011812cdf2aeab03bca64bad4c5aad07a71c35ba
Hyoungsung Kim <rktkek456@gmail.com>
2019-04-14 14:16:12 +0900

    test

---
+++ erlang/Introducing erlang/src/ask.erl
@@ -0,0 +1,18 @@
+-module(ask).
+-export([term/0]).
+
+term() ->
+    Input = io:read("What {planmo, distance} ? >> "),
+    process_term(Input).
+
+process_term({ok, Term}) when is_tuple(Term) -> 
+    Velocity = drop:fall_velocity(Term),
+    io:format("Yields ~w. ~n", [Velocity]),
+    term();    
+
+process_term({ok, quit}) ->
+    io:format("Goodbye~n");
+    %Does not call term(0 again
+
+process_term({ok,_}) -> io:format("you must enter a tuple.~n"), term();
+process_term({error, _}) -> io:format("You must enter a tuple with correct syntax.~n"), term().
