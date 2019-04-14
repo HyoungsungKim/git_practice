3a9017eceee59cc898387638588e3036ba08a47e
HS Kim <rktkek456@gmail.com>
2019-04-14 16:56:09 +0900

    pascal

--- erlang/Introducing erlang/src/drop.erl
+++ erlang/Introducing erlang/src/drop.erl
@@ -3,5 +3,6 @@
 fall_velocity({Planmo, Distance}) when element(2, {Planmo, Distance}) >= 0 ->
     case element(1, {Planmo, Distance}) of
     earth -> math:sqrt(element(2, {Planmo, Distance}) * 9.8);
-    mars -> math:sqrt(element(2, {Planmo, Distance}) * 4.9)
+    mars -> math:sqrt(element(2, {Planmo, Distance}) * 4.9);
+    moon -> math:sqrt(element(2, {Planmo, Distance}) * 6.0)
 end.
