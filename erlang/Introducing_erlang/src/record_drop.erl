2b9c5681b1f34d7150d56727502adc7c048aed65
HS Kim <rktkek456@gmail.com>
2019-04-18 02:49:19 +0900

    merge test

--- erlang/Introducing_erlang/src/record_drop.erl
+++ erlang/Introducing_erlang/src/record_drop.erl
@@ -5,6 +5,6 @@
 fall_velocity(#tower{planemo = Planemo, height = Distance}) ->
     fall_velocity(Planemo, Distance).
 
-fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
+fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 10 * Distance);
 fall_velocity(moon, Distance) when Distance >= 0 -> math:sqrt(2 * 1.6 * Distance);
 fall_velocity(mars, Distance) when Distance >= 0-> math:sqrt(2 * 3.71 * Distance).
