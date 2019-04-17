4fd469549ea18b542674816357b63f7524c80e42
HS Kim <rktkek456@gmail.com>
2019-04-18 02:03:49 +0900

    Storung structured data practice

--- erlang/Introducing_erlang/src/record_drop.erl
+++ erlang/Introducing_erlang/src/record_drop.erl
@@ -2,8 +2,8 @@
 -export([fall_velocity/1]).
 -include("records.hrl").
 
-fall_velocity(#tower{} = T) ->
-    fall_velocity(T#tower.planemo, T#tower.height).
+fall_velocity(#tower{planemo = Planemo, height = Distance}) ->
+    fall_velocity(Planemo, Distance).
 
 fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
 fall_velocity(moon, Distance) when Distance >= 0 -> math:sqrt(2 * 1.6 * Distance);
